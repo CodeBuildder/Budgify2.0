6144d6aadeabaa9b6987745df1b7c4d49f91d094
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-12-10 12:19:21 +0400

    6.1

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import { startSetExpense } from './actions/expenses'
 import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -16,4 +17,10 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+
+store.dispatch(startSetExpense()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'))
+})
+
+
