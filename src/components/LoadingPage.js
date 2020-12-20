fcc772ec00a81d3474e453a577dc06e54442a3a5
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-12-20 17:40:02 +0400

    loaderfinal

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LoadingPage = () => (
+
+    <div className="loadingPage">
+        <div className="box">
+            <div className="b b1"></div>
+            <div className="b b2"></div>
+            <div className="b b3"></div>
+            <div className="b b4"></div>
+        </div>
+        loading..
+    </div>
+
+)
+
+export default LoadingPage
